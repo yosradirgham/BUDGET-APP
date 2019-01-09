437f7efb5725e6719cc66f1f51376c35e50e4f4b
yosradirgham <dirgham.yosra@gmail.com>
2019-01-09 10:23:02 +0100

    Modules added

--- Budget App/app.js
+++ Budget App/app.js
@@ -1,40 +1,41 @@
+var budgetController = (function(){
 
+})();
+
+var userInterface = (function(){
+
+})();
+
+var dataManipulation = (function(){
+
+})();
+
+
+
+
+
+/*
 //the Date
 const d = new Date();
 const months = ['January','Febuary','Martch','April','May','June','July','August','September','October','November','December'];
 
+//add a function init 
 //available budget in ${month}
 document.querySelector('#budget-title').textContent = `Available budget in ${months[d.getMonth()]}`;
 
 //To do:
 // add an event handler in order to fetch the data the user enters, save it, and update the budget
+document.querySelector('.enter-budget-btn').addEventListener('click',() => {
+    let icnExp = new Map();
+    icnExp.set('sign', `${document.querySelector('.select').textContent}`);
+    icnExp.set(`description` ,`${document.querySelector('.enterItem').textContent}`);
+    icnExp.set(`budget` ,`${ document.querySelector('.enterMoneySpentOnTheItem').textContent}`);
+    alert( document.querySelector('.enterItem'));
+    if(document.querySelector('.select').textContent == '+'){
+    }
+    else if(document.querySelector('.select').textContent == '-'){
+        
+    }
+});
 
-
-
-/*function getMonthString(d){
-	return months[d.getMonth()]
-} */
-//document.querySelector('#date').textContent = `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}`;
-
-//Add weather + location + user Name
-
-
-//create a table with 2 indexes: one for the incomes and another one the expenses
-//when the user enters a positive amount of money we increment the incomes
-//when the user enters a negative amount of money we increment the expenses
-//when the income or expenses are equal to 0 => shouldn't be displayed
-
-let arr = [0,0];
-let getIncome = arr[0];
-let getExpenses = arr[1];
-//document.querySelector('.income-value').textContent;
-//let getExpenses = document.querySelector('.expenses-value').textContent;
-
-
-
-
-//Month budget:
-//document.querySelector('.income-value').textContent = arr[1];
-//document.querySelector('.expenses-value').textContent = arr[0];
-document.querySelector('#budget').textContent = '-0.00';
-
+*/
