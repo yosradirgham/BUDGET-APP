09e0ebe3252d5ab637e12ea57c7fe855afbfcda2
yosradirgham <dirgham.yosra@gmail.com>
2019-01-09 23:30:21 +0100

    controllers sat up

--- Budget App/app.js
+++ Budget App/app.js
@@ -1,41 +1,52 @@
+
+// Budget controller
 var budgetController = (function(){
-
+	//update the budget
+	//update the income
+	//update the expense + percentage
 })();
 
-var userInterface = (function(){
+// User Interface controller
+var UIController = (function(){
+	
+	//get input data
+	function getInputData(){
+		return{
+			sign : document.querySelector('.select').value,
+			item : document.querySelector('.enterItem').value,
+			money: document.querySelector('.enterMoneySpentOnTheItem').value
+		};
+	}
 
+	//add lists of incomes and expenses
+
+	return {
+		inputDataPublic : function(){
+			console.log(getInputData());
+		}
+	}
 })();
 
-var dataManipulation = (function(){
+// Global App Controller
+var controller = (function(budgetCtrl, UICtrl){
 
-})();
+	var ctrlAddItem = function(){
+		//1. get input data
+		UIController.inputDataPublic();
+		//2. update the budget, percentage of expenses, income or expenses depending on the sign the user entered
+		//3. display calculated data
+		//4. add the item to the UI		
+	};	
+
+	//set up the event listener for the input button 
+	document.querySelector('.enter-budget-btn').addEventListener('click',ctrlAddItem);
+	
+	//5. we want this to happen when the user hits the enter button, but also when he hits the enter key (on the keyboard)
+	document.addEventListener('keypress', function(e){
+		if(e.keyCode === 13){
+			ctrlAddItem();
+		}
+	});
 
 
-
-
-
-/*
-//the Date
-const d = new Date();
-const months = ['January','Febuary','Martch','April','May','June','July','August','September','October','November','December'];
-
-//add a function init 
-//available budget in ${month}
-document.querySelector('#budget-title').textContent = `Available budget in ${months[d.getMonth()]}`;
-
-//To do:
-// add an event handler in order to fetch the data the user enters, save it, and update the budget
-document.querySelector('.enter-budget-btn').addEventListener('click',() => {
-    let icnExp = new Map();
-    icnExp.set('sign', `${document.querySelector('.select').textContent}`);
-    icnExp.set(`description` ,`${document.querySelector('.enterItem').textContent}`);
-    icnExp.set(`budget` ,`${ document.querySelector('.enterMoneySpentOnTheItem').textContent}`);
-    alert( document.querySelector('.enterItem'));
-    if(document.querySelector('.select').textContent == '+'){
-    }
-    else if(document.querySelector('.select').textContent == '-'){
-        
-    }
-});
-
-*/
+})(budgetController, UIController);
