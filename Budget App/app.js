a52c33ce4323218cb999861f4122f36c9610a67c
yosradirgham <dirgham.yosra@gmail.com>
2019-01-10 22:25:11 +0100

    modified: app.js

--- Budget App/app.js
+++ Budget App/app.js
@@ -1,71 +1,132 @@
+	
+//update the budget
 
-// Budget controller
+//update the income
+
+//update the expense + percentage
+
+// Budget controller : keeps track of all the incomes and expenses
 var budgetController = (function(){
-
-	//update the budget
-	//update the income
-	//update the expense + percentage
-
-	return{
+	var Expense = function(id, description, value){
+		this.id          = id;
+		this.description = description;
+		this.value       = value;
 	};
+
+	var Income = function(id, description, value){
+		this.id          = id;
+		this.description = description;
+		this.value       = value;
+	};
+
+
+	var budgetObj = {
+		allItems : {
+			exp : [],
+			inc : []
+		},
+
+		totalItems : {
+			totalExp : 0,
+			totalInc : 0
+		},
+
+		budget : 0
+	};
+
+	return {
+		getBudgetObj : function(){
+			return budgetObj;
+		},
+
+		getExpense : function(){
+			return new Expense();
+		},
+
+		getIncome : function(){
+			return new Income();
+		}
+	};
+
 })();
+
 
 // User Interface controller
 var UIController = (function(){
 	
 	var DOMStrings = {
+
 		inputSign : '.select',
 		inputItem : '.enterItem',
 		inputMoney: '.enterMoneySpentOnTheItem',
-		inputBtn  : '.enter-budget-btn',
+		inputBtn  : '.enter-budget-btn'
+	
 	};
 
-	//get input data
 	var getInputData = function(){
+		
 		return{
 			sign : document.querySelector(DOMStrings.inputSign).value,
 			item : document.querySelector(DOMStrings.inputItem).value,
 			money: document.querySelector(DOMStrings.inputMoney).value
 		};
+
 	};
 
 	var getDOMStrings = function(){
+		
 		return DOMStrings;//return our private DOMStrings so we can use it later on our controller <3
+	
 	};
 
-	//add lists of incomes and expenses
-
 	return {
-		inputDataPublic : function(){
-			console.log(getInputData());
+		inputDataPublic : function(){ 
+			getInputData();
 		},
-
-		getDomStrings : function(){
+		
+		getDomStrings : function(){ 
 			return getDOMStrings();
 		}
 	}
+
 })();
+
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl){
 
-	var UIDomStrings = UIController.getDomStrings();
-
 	var ctrlAddItem = function(){
-		//1. get input data
 		UIController.inputDataPublic();
-		//2. update the budget, percentage of expenses, income or expenses depending on the sign the user entered
-		//3. display calculated data
-		//4. add the item to the UI		
 	};	
 
-	document.querySelector(UIDomStrings.inputBtn).addEventListener('click',ctrlAddItem);
+	//a function where all of our event listeners will be placed
+	var setUpEventListeners = function(){
+		var UIDomStrings = UIController.getDomStrings();
+		
+		document.querySelector(UIDomStrings.inputBtn).addEventListener('click',ctrlAddItem);
+		
+		document.addEventListener('keypress', function(e){
+			//using which proprety for old browsers
+			if(e.keyCode === 13 || e.which === 13) ctrlAddItem();
+		});
+	};
 
-	document.addEventListener('keypress', function(e){
-		if(e.keyCode === 13){
-			ctrlAddItem();
+	//1. get input data
+	
+	//2. update the budget, percentage of expenses, income or expenses depending on the sign the user entered
+
+	//3. display calculated data
+
+	//4. add the item to the UI	
+	
+	return {
+
+		init : function(){
+			setUpEventListeners();
 		}
-	});
 
+	};
 
 })(budgetController, UIController);
+
+controller.init();
